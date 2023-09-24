7b495858e06d9502308328d44a0dd5c33385ea05
Bogdan <blubinskyj@gmail.com>
2023-09-24 17:54:16 +0300

    create request

--- flask-server/server.py
+++ flask-server/server.py
@@ -8,11 +8,25 @@ CORS(app)
 
 @app.route('/', methods = ['POST'])
 def cars():
-    res = request.json
-    data = jsonify(res)
+    if request.method == 'POST':
+        try:
+            # Отримайте дані у форматі JSON з тіла запиту
+            data = request.get_json()
 
-    return data
-        
+            # Доступ до даних з форми
+            brand = data.get('brand')
+            model = data.get('model')
+
+            # Вивести дані в консоль
+            print(f'Ім\'я: {brand}')
+            print(f'Email: {model}')
+
+            # Повернути відповідь
+            return jsonify({'message': 'Дані з форми були успішно отримані.'}), 200
+        except Exception as e:
+            return jsonify({'error': str(e)}), 400
+
+
 # https://auto.ria.com/uk/search/?indexName=auto,order_auto,newauto_search&categories.main.id=1&brand.id[0]=84&model.id[0]=35449&year[0].gte=1900&year[0].lte=2023&price.USD.gte=1&price.USD.lte=100000000&region.id[0]=5&city.id[0]=5
 # brand.id[0]=84
 # model.id[0]=35449
